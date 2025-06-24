92267a523fa3cc6a16a00f95320f4c155919da80
Jackie Merritt <remmerritt@gmail.com>
2025-06-24 15:43:10 -0400

    Assignment fully complete

--- Chp3-Lab2.py
+++ Chp3-Lab2.py
@@ -1,16 +1,23 @@
 #Jackie-Merritt_Lab2/Chp3-6/24/2025
 
-#Inputs and Variabls
+#Initial inputs
+
 t = 0
 print("Jane's Stuff Store")
 print()
 p = int(input("How many items would you like to purchase? "))
 print()
-for num in range(1, p + 1):
-    f = input(f'Enter the price of item {num}: ')
+
+#Math and loop
+
+for num in range(p):
+    f = input(f'Enter the price of item {num + 1}: ')
     t = float(f) + t
-    av = t / num
-    print()
+av = t / num
+print()
+
+#output results
+    
 print(f'The total cost of your items is ${t:.2f}')
 print(f'The average cost of each item is ${av:.2f}')
 
